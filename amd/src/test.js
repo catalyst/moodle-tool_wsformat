b57c36f2517fd9bef8b78acc9066b4c3b4c0e384
djarran <me@djarran.co>
2023-08-13 21:33:31 +1000

    Move javascript from template to test.js

--- amd/src/test.js
+++ amd/src/test.js
@@ -1,3 +1,22 @@
 export const init = () => {
     window.alert("IT WORKSsssshhhhhkl");
 };
+
+
+const webserviceListItems = document.querySelectorAll('li[data-name]');
+const placeholderText = document.querySelector('#placeholder-text');
+const wsDescriptionPanel = document.querySelector('#ws-panel');
+
+webserviceListItems.forEach(function (webserviceItem) {
+
+    webserviceItem.addEventListener('click', function () {
+        const nameAttr = webserviceItem.getAttribute("data-name");
+        console.log(nameAttr);
+
+        if (placeholderText) {
+            placeholderText.remove();
+        }
+        wsDescriptionPanel.style.display = "flex";
+
+    });
+});
