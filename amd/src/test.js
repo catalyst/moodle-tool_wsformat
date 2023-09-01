85731a12134985768db7eb3aa0516b3c6c271fdc
djarran <--unset>
2023-09-01 18:52:52 +1000

    Fix eslint errors

--- amd/src/test.js
+++ amd/src/test.js
@@ -1,25 +1,14 @@
-import * as autocomplete from 'core/form-autocomplete'
-
-
 export const init = () => {
-    // window.alert("IT WORKSsssshhhhhkl");
-    console.log("wow")
-    console.log(autocomplete);
 };
 
 const webserviceListItems = document.querySelectorAll('li[data-name]');
 const placeholderText = document.querySelector('#placeholder-text');
 const wsDescriptionPanel = document.querySelector('#ws-panel');
-setTimeout(() => {
-    const selection = document.querySelector('.form-autocomplete-selection')
-    // selection.style.display = 'none';
-}, 5000);
 
 webserviceListItems.forEach(function (webserviceItem) {
 
     webserviceItem.addEventListener('click', function () {
-        const nameAttr = webserviceItem.getAttribute("data-name");
-        console.log(nameAttr);
+        webserviceItem.getAttribute("data-name");
 
         if (placeholderText) {
             placeholderText.remove();
