840d4ef5e95f089921f2020a4a04621053035293
ZachPregl <zpregl1@gmail.com>
2023-09-19 21:47:40 +1000

    Partially working, not there yet, will be tomorrow

--- amd/src/test.js
+++ amd/src/test.js
@@ -1,5 +1,7 @@
 export const init = () => {
+    setUpCopyFunctionality();
 };
+
 
 //console.log("Helloooo this is Jackie");
 const webserviceListItems = document.querySelectorAll('li[data-name]');
@@ -10,6 +12,27 @@ let panelTitle = document.getElementById('ws-title');
 let panelDesc = document.getElementById('ws-description');
 let panelCurl = document.getElementById('ws-curl');
 
+
+const setUpCopyFunctionality = () => {
+    const copyButton = document.querySelector('button[onclick="copyCurlContent()"]');
+
+    if (copyButton) {
+        copyButton.removeAttribute('onclick');  
+        copyButton.addEventListener('click', copyCurlContent);
+    }
+};
+
+function copyCurlContent() {
+    console.log("Helloooo this is Zachie");
+    const textToCopy = document.getElementById('ws-curl').textContent.trim();
+    const textarea = document.createElement('textarea');
+    textarea.value = textToCopy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert('Content copied to clipboard!');
+}
 webserviceListItems.forEach(function (webserviceItem) {
 
     webserviceItem.addEventListener('click', (e) => {
