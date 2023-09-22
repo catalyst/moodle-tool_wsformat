8fb963bc3cba178f5c3778f83fcd214f03ccbf42
djarran <--unset>
2023-09-22 12:15:43 +1000

    Fix error with copying cURL command

--- amd/src/test.js
+++ amd/src/test.js
@@ -1,6 +1,6 @@
 export const init = () => {
     setUpCopyFunctionality();
-    
+
 };
 
 
@@ -9,37 +9,20 @@ const webserviceListItems = document.querySelectorAll('li[data-name]');
 const placeholderText = document.querySelector('#placeholder-text');
 const wsDescriptionPanel = document.querySelector('#ws-panel');
 
+
 let panelTitle = document.getElementById('ws-title');
 let panelDesc = document.getElementById('ws-description');
 let panelCurl = document.getElementById('ws-curl');
 
 
- const setUpCopyFunctionality = () => {
-    const copyButton = document.querySelector('button[onclick="copyCurlContent()"]');
-    
-    if (copyButton) {
-        copyButton.removeAttribute('onclick');  
-        copyButton.addEventListener('click', copyCurlContent);
-    }
-}; 
+const curlCopyButton = document.querySelector('#curl-copy-button');
+curlCopyButton.addEventListener('click', copyCurlContent)
 
-function testClick() {
-    console.log('Button was clicked');
-}
 function copyCurlContent() {
-    console.log("Test");
-    alert("It worked");
-    let curlContent = document.getElementById(data-curl).textContent;
-
-    let textArea = document.createElement("textarea");
-    textArea.value = curlContent;
-    document.body.appendChild(textArea);
-    textArea.select();
-
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-    alert("It worked");
+    let curlContent = document.getElementById('ws-curl').innerText;
+    navigator.clipboard.writeText(curlContent);
 }
+
 webserviceListItems.forEach(function (webserviceItem) {
 
     webserviceItem.addEventListener('click', (e) => {
@@ -47,14 +30,14 @@ webserviceListItems.forEach(function (webserviceItem) {
         const name = webserviceItem.getAttribute("data-name");
         const description = webserviceItem.getAttribute("data-desc");
         const curl = webserviceItem.getAttribute("data-curl");
-     
+
         if (placeholderText) {
             placeholderText.remove();
         }
 
         wsDescriptionPanel.style.display = "flex";
 
-        panelTitle.innerHTML= name;
+        panelTitle.innerHTML = name;
         panelDesc.innerHTML = description;
         panelCurl.innerHTML = curl;
 
